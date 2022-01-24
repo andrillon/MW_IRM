bb32da7a5d575ec5bbf7da15fdb5a855b25c07a1
Thomas Andrillon <31892797+andrillon@users.noreply.github.com>
2022-01-24 11:02:56 +0100

    adding a readme

---
+++ readme.m
@@ -0,0 +1,28 @@
+ % probe
+    %  1: num probe within block
+    %  2: probe onset (expected)
+    %  3: probe onset (actual)
+    %  4: num block
+    %  5: block type (1: digit / 2: emotions / 3: gender)
+    %  6: num trial
+    %  7: resp Q1 Mind-State (1: On / 2: MW / 3: MB / 4: ?)
+    %  8: resp Q2 Origin (1: room / 2: personal / 3: task)
+    %  9: resp Q3 Alterness (1 (alert) to 4 (sleepy))
+    % 10-12: onset R1-3
+    % 13-15: onset Q1-3
+    % 16-18: resp number Q1-3
+% test
+    %  1: num block
+    %  2: block cond (1: digit / 2: emotions / 3: gender)
+    %  3: image set
+    %  4: num trial
+    %  5: seq trial
+    %  6: target
+    %  7: resp
+    %  8: stim onset
+    %  9: stim pre
+    % 10: resp onset
+    % 11: nogo
+    % 12: go
+ 
+% slow wave
